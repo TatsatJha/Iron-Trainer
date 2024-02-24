63ce4e81cde9423b1844b167154b76a48663342c
TatsatJha <TatsatTarun@gmail.com>
2024-02-23 23:26:33 -0700

    Rudimentary mock ups of ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,33 @@ import './App.css'
 
 export default function App(){
   return(
-    <h1 className='text-5xl underline'>
-      Iron Trainer!
-    </h1>
+    // break up into components
+    <>
+      <div className='bg-zinc-600 top-0 fixed w-screen '>
+        <h1 className='text-2xl p-4 flex justify-center'>
+            Iron-Trainer!
+        </h1>
+        <nav className='flex justify-center text-xl '>
+          <h2 className='p-2 px-12 border-solid border-black border-2'>Home</h2>
+          <h2 className='p-2 px-12 border-solid border-black border-2'>My Programs</h2>
+          <h2 className='p-2 px-12 border-solid border-black border-2'>Progress</h2>
+          <h2 className='p-2 px-12 border-solid border-black border-2'>Discover</h2>
+        </nav>
+        
+      </div>
+      <main className='h-100% bg-slate-800 mt-28'>
+        <p className='text-3xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam varius eget arcu quis fermentum. Cras nec diam sit amet diam finibus pulvinar a at eros. Aliquam lobortis posuere rhoncus. Curabitur tempus justo quis dignissim luctus. Vestibulum vitae tincidunt ex, quis auctor ante. Fusce efficitur sem sit amet tortor pretium, a auctor odio fringilla. Ut rutrum elit ac turpis sodales euismod. In egestas ultrices augue scelerisque cursus. Aliquam tempor aliquet dolor, in consequat ex cursus vel. Morbi nec libero urna. Etiam euismod tellus purus, eget ultricies elit pulvinar eu. Phasellus semper eleifend arcu, nec eleifend lectus. Quisque metus nunc, dignissim quis nibh vitae, tempor lacinia tortor. Nullam nunc urna, feugiat non justo sit amet, iaculis dignissim velit. Aenean feugiat lectus nisl, sit amet semper ligula facilisis in. Nulla mattis mattis metus.
+
+Maecenas dignissim ultricies rutrum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc dignissim interdum turpis, quis laoreet risus egestas at. Donec sollicitudin facilisis enim, in pharetra enim cursus gravida. Quisque scelerisque nunc gravida, tempor felis eget, pharetra urna. Morbi sit amet velit faucibus, pellentesque nisi ornare, facilisis elit. Phasellus viverra lacinia leo, eu placerat libero vehicula tempor. Maecenas sed mauris quis dui luctus commodo eu eu nisi. Quisque vitae nunc in libero venenatis rhoncus at ac lorem. Duis hendrerit luctus pellentesque. Proin egestas libero eros, non posuere nunc elementum iaculis. Duis nec dui sit amet nibh sollicitudin fringilla. Phasellus nec enim felis.
+
+Vestibulum quis felis pellentesque, pulvinar nisl ut, gravida nibh. Sed lacinia pulvinar pharetra. Donec lacinia scelerisque augue et fermentum. Ut vitae tortor pharetra, tempor velit ut, vestibulum risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut quam nulla, viverra quis eleifend nec, vehicula id tortor. Suspendisse efficitur ornare justo molestie bibendum. Nulla malesuada gravida enim, in mollis tellus rutrum eget. Sed massa erat, tincidunt in volutpat sed, blandit non eros. Cras facilisis dictum sem suscipit convallis. Ut vel placerat odio. Vestibulum ac massa augue. Cras dui erat, vulputate nec nibh nec, elementum fringilla tellus. Pellentesque imperdiet neque ornare dui sodales, ac varius risus dignissim. Nunc quis orci sit amet ex molestie convallis non ut tortor. Donec sed rhoncus ligula, et volutpat lacus.
+
+Integer vestibulum dignissim metus sed posuere. Sed hendrerit massa elit. Donec mauris nibh, tincidunt tincidunt elementum eget, vulputate a ipsum. Suspendisse a suscipit elit. Ut quis placerat arcu, quis laoreet massa. Pellentesque vestibulum in leo sed ultricies. Nullam sit amet pretium massa.
+
+Integer dolor sem, porttitor sed vehicula nec, facilisis a sapien. Fusce in mi nec lacus ultricies vehicula. Aenean urna diam, finibus non nulla a, pulvinar rhoncus risus. Nunc et dui tortor. Nullam dolor tortor, congue non hendrerit quis, venenatis lobortis odio. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus venenatis, nunc eleifend egestas ultrices, est augue scelerisque diam, at auctor quam neque ac nisi. Pellentesque pretium velit finibus faucibus sodales. Donec et commodo ex. Fusce sed mauris vel ante consectetur finibus. Cras bibendum turpis dolor, a posuere tortor feugiat at. Vestibulum ac felis et augue elementum rutrum. In hac habitasse platea dictumst. Fusce purus justo, luctus non libero vel, scelerisque aliquet tellus.</p>
+        </main>
+{/* routing needed to dynamically display content in the future */}
+      
+    </>
   )
 }
