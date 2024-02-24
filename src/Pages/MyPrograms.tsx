b198425e233d76ae72c5e1bf5cccaccc038d1468
TatsatJha <TatsatTarun@gmail.com>
2024-02-24 16:09:39 -0700

    implement grid layout in my programs

--- src/Pages/MyPrograms.tsx
+++ src/Pages/MyPrograms.tsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function MyPrograms() {
   return (
-    <div>MyPrograms</div>
+    <>
+      <main className='h-screen w-screen bg-[#9DB2BF] mt-16 '>
+        <div className='h-screen grid grid-cols-3 pb-24'>
+          <p className='border-solid m-12 border-black border-2 bg-[#DDE6ED] '>
+
+          </p>
+
+        </div>
+        <p>+</p>
+      </main>
+    </>
   )
 }
