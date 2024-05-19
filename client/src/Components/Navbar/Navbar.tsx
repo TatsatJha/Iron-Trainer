19864133b686c9da7d36cee8310053ce983542a3
TatsatJha <TatsatTarun@gmail.com>
2024-05-19 16:14:17 -0700

    update NavBar styling

--- client/src/Components/Navbar/Navbar.tsx
+++ client/src/Components/Navbar/Navbar.tsx
@@ -2,17 +2,17 @@ import Navbutton from "./Navbutton";
 
     export default function Navbar() {
       return (
-
-        <div className="top-0 fixed w-screen px-24 text-violet-800 font-bold border-b-violet-800 border-[2px] flex justify-between z-50 bg-white">
+        <div className="top-0 fixed w-screen px-24 text-violet-800 font-bold flex justify-between z-50 bg]">
           <h1>
             <Navbutton to = "/" name ="Iron-Trainer" style="text-2xl p-4 flex justify-start"/>
           </h1>
           <nav className='flex justify-evenly text-xl'>
-            <Navbutton to = "/" name ="Home" style={"p-4 px-12"}></Navbutton>
-            <Navbutton to = "/programs" name ="My Programs" style={"p-4 px-12"}></Navbutton>
-            <Navbutton to = "/progress" name ="Progress" style={"p-4 px-12"}></Navbutton>
-            <Navbutton to = "/discover" name ="Discover" style={"p-4 px-12"}></Navbutton>
+            <Navbutton to = "/" name ="Home" style={"p-4 px-8"}></Navbutton>
+            <Navbutton to = "/programs" name ="My Programs" style={"p-4 px-8"}></Navbutton>
+            <Navbutton to = "/progress" name ="Progress" style={"p-4 px-8"}></Navbutton>
+            <Navbutton to = "/discover" name ="Discover" style={"p-4 px-8"}></Navbutton>
           </nav>
+          <button className="px-6 m-2 border-2 h-auto border-violet-600 rounded-lg text-lg font-light">Log in</button>
         </div>
       );
     }
