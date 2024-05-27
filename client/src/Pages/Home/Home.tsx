df695ec80119686924a4fc305395d6e4de9b9184
TatsatJha <TatsatTarun@gmail.com>
2024-05-26 23:16:58 -0700

    begin scaffolding home page

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <>
       <Main name = "dyanmic content!"></Main>
+      <div>Current Program</div>
+      <div>Current Stage: Beginner</div>
+      <div>Trophies:</div>
+      <div>Level:</div>
     </>
   )
 }
