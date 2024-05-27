f96e0c23e18706efcea8e36c9045eacd8dc93e91
TatsatJha <TatsatTarun@gmail.com>
2024-05-26 21:53:52 -0700

    update styling

--- client/src/Components/Navbar/Navbar.tsx
+++ client/src/Components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Navbutton from "./Navbutton";
+import { IoReorderThreeOutline } from "react-icons/io5";
 
     export default function Navbar() {
       const [isMobile, setIsMobile] = useState(window.innerWidth < 600)
@@ -11,10 +12,18 @@ import Navbutton from "./Navbutton";
 
       return (
         (isMobile) ? 
-        <div className="flex top-0 fixed w-screen justify-end z-50 bg-white">
-          <button className="p-4 bg-emerald-600 text-emerald-900 rounded-lg m-4 bold"> Download App</button>
+        <div className="flex top-0 fixed w-screen align-middle justify-between z-50 bg-white">
+          <h1>
+            <Navbutton to = "/" name ="Iron-Trainer" style="text-2xl p-4 flex justify-start"/>
+          </h1>
+          <button className="px-6 h-fit my-auto py-2 bg-emerald-600 text-white rounded-lg m-2 font-bold"> Download App</button>
+          <button className="text-3xl pr-6">
+             <IoReorderThreeOutline></IoReorderThreeOutline>
+          </button>
         </div>
+
         :
+
         <div className="top-0 fixed flex w-screen px-24 text-emerald-600 font-bold justify-between z-50 bg-white p-2 ">
           <h1>
             <Navbutton to = "/" name ="Iron-Trainer" style="text-2xl p-4 flex justify-start"/>
@@ -23,7 +32,7 @@ import Navbutton from "./Navbutton";
             <Navbutton to = "/" name ="Home" style={"p-4 px-8"}></Navbutton>
             <Navbutton to = "/my-programs" name ="My Programs" style={"p-4 px-8"}></Navbutton>
             <Navbutton to = "/discover" name ="Discover" style={"p-4 px-8"}></Navbutton>
-            <button className="px-6 m-2 border-2 h-auto border-emerald-400 rounded-lg text-lg font-light">Log in</button>
+            <button className="px-6 m-2 border-2 h-auto border-emerald-400 rounded-lg text-lg font-bold">Log in</button>
           </nav>
         </div>
          
