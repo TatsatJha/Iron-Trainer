22f76a0e66fe7baf866fd6b342db2e9658bd8316
TatsatJha <TatsatTarun@gmail.com>
2024-06-23 22:52:44 -0700

    Finish making ui dynamic

--- client/src/Components/chat/Message.tsx
+++ client/src/Components/chat/Message.tsx
@@ -1,8 +1,10 @@
+import { Avatar } from "@mui/material";
+
 export default function Message(props:{text: string}) {
   return (
-    <div className="border-gray-100 shadow rounded-xl border-solid border-2 p-4 w-fit">
-        <p>avatar image</p>
-        <p>{props.text}</p>
+    <div className="border-gray-100 shadow rounded-xl border-solid border-2 w-fit flex py-4 pr-8">
+        <Avatar className="m-4"></Avatar>
+        <p className="my-auto">{props.text}</p>
     </div>
   )
 }
--- client/src/Pages/chat/Chat.tsx
+++ client/src/Pages/chat/Chat.tsx
@@ -1,17 +1,47 @@
+import { useCallback, useState } from "react"
 import Message from "../../components/chat/Message.tsx"
+import { Send } from "@mui/icons-material"
 
 export default function Chat() {
+    const [chatHistory, setChatHistory] = useState<Array<string>>([])
+    const [prompt, setPrompt] = useState("")
+
+    const sendPrompt = ()=>{
+        // update chat history
+            // check if the message is empty
+            if(prompt.replace(/\s/g,'') == ""){return}
+            // append prompt
+            setChatHistory([...chatHistory, prompt])
+            // setprompt to empty string
+            setPrompt("")
+            
+
+        // send api request
+    }
+    const updatePrompt = (e: any)=>{
+        setPrompt(e.target.value)
+    }
+    const renderMessage = useCallback(
+        (message: string)=>{
+        return(
+            <Message text={message}></Message>
+        )
+        },[]
+    )
   return (
     <div className='h-screen w-screen'>
         <div className="mt-20 p-8 w-3/4 mx-auto shadow h-full flex-col relative">
-            <Message text="A bunch of really long text to be honest with you we can just test this as a thing for practicing how fast we can talk cause we can still get better at that  to be honest with you"></Message>
-            <Message text="A bunch of really long text to be honest with you we can just test this as a thing for practicing how fast we can talk cause we can still get better at that  to be honest with you"></Message>
-            <Message text="A bunch of really long text to be honest with you we can just test this as a thing for practicing how fast we can talk cause we can still get better at that  to be honest with you"></Message>
-            <Message text="A bunch of really long text to be honest with you we can just test this as a thing for practicing how fast we can talk cause we can still get better at that  to be honest with you"></Message>
-
+        {
+            chatHistory.map((message) => renderMessage(message))
+        }
         </div>
-        <input className="fixed bottom-0 left-1/4 p-6 w-1/2 rounded-lg border-black border-solid border-2 mb-8" placeholder="Ask VAI" type="text">
-        </input>
+        <div className="fixed bottom-0 left-1/4 w-full">
+            <input className="p-6 w-1/2 rounded-lg border-black border-solid border-2 mb-8 mr-4" placeholder="Ask VAI" type="text" value={prompt} onChange={updatePrompt}>
+            </input>
+            <span className="bg-blue-300 p-4 rounded border-blue-500 border-solid border-2">
+            <Send onClick={sendPrompt}></Send>
+            </span>
+        </div>
     </div>
   )
 }
