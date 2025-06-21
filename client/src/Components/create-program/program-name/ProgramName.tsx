1036ebc77a49f974f2eabbdd606a8e2a55f7b140
TatsatJha <TatsatTarun@gmail.com>
2025-06-20 17:16:07 -0700

    Widen Program Name

--- client/src/Components/create-program/program-name/ProgramName.tsx
+++ client/src/Components/create-program/program-name/ProgramName.tsx
@@ -1,12 +1,12 @@
 export default function ProgramName({title, setTitle}: {title: string, setTitle: Function}) {
   return (
     <>
-    <div className="group w-fit mx-auto mt-8">
+    <div className="group mx-auto mt-8">
       <input 
         type='text' 
         value={title} 
         onChange={(e) => setTitle(e.target.value)} 
-        className='text-center text-4xl font-bold block mx-auto outline-none text-gray-800 bg-transparent p-4'
+        className='text-center text-4xl font-bold block mx-auto outline-none text-gray-800 bg-transparent p-4 w-screen'
         placeholder="Enter Program Name"
       />
 
